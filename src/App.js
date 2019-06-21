4fb3e2feafff44c351e3e38bd7bde68062624109
Jicheol Oh <jicheol@gmail.com>
2019-06-20 17:30:31 -0700

    1-9. CounterContainer 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from './components/Counter'
 import CounterOld from './components/CounterOld'
+import CounterContainer from './containers/CounterContainer'
 function App() {
   return (
     <div className="App">
       <Counter/>
+      <CounterContainer/>
       <CounterOld/>
     </div>
   );
